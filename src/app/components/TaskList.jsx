432feee5a5a23a4e762260a743be1c916ea43b6f
Ahmed Abdelhay <abdelhay@192.168.1.3>
2019-05-24 16:31:16 +0200

    first flight

---
+++ src/app/components/TaskList.jsx
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+export const TaskList = ({tasks, name}) => (
+	<div>
+		<h3>{name}</h3>
+		<div>
+			{tasks.map((task) => (
+				<div key={task.id}>{task.name}</div>
+			))}
+		</div>
+	</div>
+);
+
+function mapStateToProps(state, ownProps) {
+	let groupId = ownProps.id;
+	return {
+		name: ownProps.name,
+		id: groupId,
+		tasks: state.tasks.filter((task) => task.group === groupId)
+	};
+}
+
+export const ConnectedTaskList = connect(
+	mapStateToProps,
+)(TaskList);
